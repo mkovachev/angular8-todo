f66c89b0f9ecff853f40521d36eb99c3e1703842
mkovachev <mkovachev6@gmail.com>
2020-01-07 12:08:08 +0200

    added todo-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TodosComponent } from './components/todos/todos.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TodosComponent } from "./components/todos/todos.component";
+import { TodoItemComponent } from "./components/todo-item/todo-item.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodosComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodosComponent, TodoItemComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
